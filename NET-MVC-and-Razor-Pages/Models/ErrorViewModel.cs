da517d05c596b953b598b362759a15a802f5fe3c
Muhammad Junaid <muhammad.junaid@codeninjaconsulting.com>
2023-10-17 16:07:49 +0500

    InitialSetup

---
+++ NET-MVC-and-Razor-Pages/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET_MVC_and_Razor_Pages.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
