92dad84fb2fd755396ac06f62aa84a4b62b676c0
Muhammad Junaid <muhammad.junaid@codeninjaconsulting.com>
2023-10-17 17:00:05 +0500

    Add infrastructure

--- NET-MVC-and-Razor-Pages/Program.cs
+++ NET-MVC-and-Razor-Pages/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>()
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
---
+++ Task4.Infrastructure/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NET_MVC_and_Razor_Pages.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
