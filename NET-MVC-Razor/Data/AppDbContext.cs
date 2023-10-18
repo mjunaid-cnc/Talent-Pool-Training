9cb350f0767625884fcc19f8a78690421cdac720
Muhammad Junaid <muhammad.junaid@codeninjaconsulting.com>
2023-10-18 11:36:46 +0500

    New project initial setup

---
+++ NET-MVC-Razor/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NET_MVC_and_Razor_Pages.Models;
+
+namespace NET_MVC_Razor.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
