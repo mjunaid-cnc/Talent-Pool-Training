713d2d1d2354b31e8aa2f094686732760bb16902
Muhammad Junaid <muhammad.junaid@codeninjaconsulting.com>
2023-10-25 16:23:23 +0500

    Employee model added

--- NET-MVC-Razor/Migrations/20231025094900_Employee.Designer.cs
+++ NET-MVC-Razor/Migrations/20231025094900_Employee.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NET_MVC_Razor.Data;
 
@@ -11,9 +12,11 @@ using NET_MVC_Razor.Data;
 namespace NET_MVC_Razor.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231025094900_Employee")]
+    partial class Employee
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ NET-MVC-Razor/Models/Domain/Employee.cs
@@ -0,0 +1,20 @@
+﻿using NET_MVC_Razor.CustomAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace NET_MVC_Razor.Models.Domain
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Salary { get; set; }
+        [EmailAddress]
+        [Required]
+        [TestEmail]
+        public string Email { get; set; }
+        public User? User { get; set; }
+    }
+}
