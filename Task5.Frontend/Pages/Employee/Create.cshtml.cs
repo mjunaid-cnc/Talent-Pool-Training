101680d743da54f567441637c5a811015a933588
Muhammad Junaid <muhammad.junaid@codeninjaconsulting.com>
2023-11-02 15:19:36 +0500

    Task 5: Add Razor pages

---
+++ Task5.Frontend/Pages/Employee/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Task5.Frontend.Pages.Employee
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
