7d64e8995b8e54ccb8bda48776784d76f48896fb
Muhammad Junaid <muhammad.junaid@codeninjaconsulting.com>
2023-11-27 16:33:38 +0500

    Initial setup

---
+++ Task9.Frontend/Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Task9.Frontend.Pages.Employees
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
